182bc6762b4ec51e4ca732d5a95b3fe16d958a0a
MarcelaJI <ingridjimenez113@gmail.com>
2025-04-30 11:49:58 +0200

    Adding .h

---
+++ includes/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ingjimen <ingjimen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 11:48:14 by ingjimen          #+#    #+#             */
+/*   Updated: 2025/04/30 11:49:31 by ingjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+# define PHILO_MAX 300
+
+#endif
