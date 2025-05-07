cc2789cf2caa79cc1a4d2ab6aa8aed5aeb2f3fed
MarcelaJI <ingridjimenez113@gmail.com>
2025-05-07 09:01:19 +0200

    Adding init_forks

--- src/ft_init_sim.c
+++ src/ft_init_sim.c
@@ -6,9 +6,23 @@
 /*   By: ingjimen <ingjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 08:36:11 by ingjimen          #+#    #+#             */
-/*   Updated: 2025/05/07 08:36:29 by ingjimen         ###   ########.fr       */
+/*   Updated: 2025/05/07 08:52:39 by ingjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
+void    init_forks(t_sim *sim)
+{
+    int     i;
+
+    i = 0;
+    sim->forks = malloc(sizeof(pthread_mutex_t) * sim->num_of_philos);
+    if (!sim->forks)
+        error_exit("Fork initialization failed\n");
+    while (i < sim->num_of_philos)
+    {
+        pthread_mutex_init(&sim->forks[i], NULL);
+        i++;
+    }
+}
