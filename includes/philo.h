799b148aaa306645421aaa61b696d467b4b94619
MarcelaJI <ingridjimenez113@gmail.com>
2025-05-09 11:20:17 +0200

    Adding take_forks

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: ingjimen <ingjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 11:48:14 by ingjimen          #+#    #+#             */
-/*   Updated: 2025/05/09 11:02:09 by ingjimen         ###   ########.fr       */
+/*   Updated: 2025/05/09 11:19:32 by ingjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,6 +82,7 @@ void		start_threads(t_sim *sim);
 void		*philo_routine(void *arg);
 long		get_elapsed_ms(t_sim *sim);
 void	print_status(t_philo *philo, char *message, char *color);
+void    take_forks(t_philo *philo);
 
 
 #endif
--- src/philo_routine.c
+++ src/philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: ingjimen <ingjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 21:02:12 by ingjimen          #+#    #+#             */
-/*   Updated: 2025/05/08 21:10:13 by ingjimen         ###   ########.fr       */
+/*   Updated: 2025/05/09 11:18:46 by ingjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,12 @@ void    *philo_routine(void *arg)
     printf("%ld %d has started\n", get_elapsed_ms(philo->sim), philo->id);
     usleep(1000);
     return (NULL);
+}
+
+void    take_forks(t_philo *philo)
+{
+    pthread_mutex_lock(philo->l_fork);
+    print_status(philo, "has taken a fork", CYAN);
+    pthread_mutex_lock(philo->r_fork);
+    print_status(philo, "has taken a fork", CYAN);
 }
