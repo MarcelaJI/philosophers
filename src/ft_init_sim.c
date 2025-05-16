1f8f535b13695e4bcf0ae3a5267d172f663cdc89
MarcelaJI <ingridjimenez113@gmail.com>
2025-05-16 09:58:25 +0200

    Adding init_last_meals

--- src/ft_init_sim.c
+++ src/ft_init_sim.c
@@ -6,27 +6,24 @@
 /*   By: ingjimen <ingjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 08:36:11 by ingjimen          #+#    #+#             */
-/*   Updated: 2025/05/09 13:47:32 by ingjimen         ###   ########.fr       */
+/*   Updated: 2025/05/16 09:54:39 by ingjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void    init_forks(t_sim *sim)
+void init_forks(t_sim *sim)
 {
-    int     i;
+	int	i;
 
-    sim->forks = malloc(sizeof(pthread_mutex_t) * sim->num_of_philos);
-    if (!sim->forks)
-        error_exit("Fork initialization failed\n");
-    i = 0;
-    while (i < sim->num_of_philos)
-    {
-        pthread_mutex_init(&sim->forks[i], NULL);
-        i++;
-    }
-    pthread_mutex_init(&sim->write_lock, NULL);
-    pthread_mutex_init(&sim->dead_lock, NULL);
+	sim->forks = malloc(sizeof(pthread_mutex_t) * sim->num_of_philos);
+	if (!sim->forks)
+		error_exit("Fork initialization failed\n");
+	i = 0;
+	while (i < sim->num_of_philos)
+		pthread_mutex_init(&sim->forks[i++], NULL);
+	pthread_mutex_init(&sim->write_lock, NULL);
+	pthread_mutex_init(&sim->dead_lock, NULL);
 }
 /*Estoy utlizando tenedor circular porque los filósofos están en un círculo. El módulo hace
 que el índice "Vuelva al principio" cuando llega al final, simulando esa forma circular en la mesa.
@@ -40,22 +37,21 @@ static void     assign_forks(t_philo *philo, int index, t_sim *sim)
 	philo->r_fork = &sim->forks[(index + 1) % sim->num_of_philos];
 }
 
-void     init_philos(t_sim *sim)
+void init_philos(t_sim *sim)
 {
-    int     i;
+	int	i = 0;
 
-    sim->philos = malloc(sizeof(t_philo) * sim->num_of_philos);
-    if (!sim->philos)
-        error_exit("Failed to allocate memory for philosophers\n");
-    i = 0;
-    while (i < sim->num_of_philos)
-    {
-        sim->philos[i].id = i + 1; // Le asigno un ID humano (empiza en 1, no en 0)
-        sim->philos[i].meals_eaten = 0; // Inicializo el número de comidas a 0
-        sim->philos[i].last_meal = 0; // Lo pongo en 0 y se actualiza cuando empiece la simulación
-        sim->philos[i].sim = sim; // guardo un puntero al struct global, por si lo necesito dentro de su rutina. Así no tengo que pasar mil argumentos en todas las funciones.
-        pthread_mutex_init(&sim->philos[i].mutex, NULL);
-        assign_forks(&sim->philos[i], i, sim);
-        i++;
-    }
+	sim->philos = malloc(sizeof(t_philo) * sim->num_of_philos);
+	if (!sim->philos)
+		error_exit("Failed to allocate memory for philosophers\n");
+	while (i < sim->num_of_philos)
+	{
+		sim->philos[i].id = i + 1;
+		sim->philos[i].meals_eaten = 0;
+		sim->philos[i].last_meal = 0;
+		sim->philos[i].sim = sim;
+		pthread_mutex_init(&sim->philos[i].mutex, NULL);
+		assign_forks(&sim->philos[i], i, sim);
+		i++;
+	}
 }
--- src/philo_routine_utils.c
+++ src/philo_routine_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo_routine_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ingjimen <ingjimen@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: ingjimen <ingjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 10:43:40 by ingjimen          #+#    #+#             */
-/*   Updated: 2025/05/14 09:01:53 by ingjimen         ###   ########.fr       */
+/*   Updated: 2025/05/16 09:57:18 by ingjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,23 @@ void release_forks(t_philo *philo)
 bool should_continue(t_philo *philo)
 {
     return (!philo_has_died(philo));
+}
+
+void init_last_meals(t_sim *sim)
+{
+	int		i;
+	long	start_ms;
+    start_ms = get_elapsed_ms(sim);
+
+    i = 0;
+	while (i < sim->num_of_philos)
+	{
+		pthread_mutex_lock(&sim->philos[i].mutex);
+		sim->philos[i].last_meal = start_ms;
+		pthread_mutex_unlock(&sim->philos[i].mutex);
+		printf("[DEBUG] Philo %d starts with last_meal = %ld\n",
+			sim->philos[i].id, start_ms);
+        i++;
+	}
 }
 
