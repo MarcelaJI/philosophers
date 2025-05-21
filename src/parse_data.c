d27cc8d7a2e9d250b6393fcbdfa17a89011760f5
MarcelaJI <ingridjimenez113@gmail.com>
2025-05-21 21:30:17 +0200

    Adding parse_data

---
+++ src/parse_data.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_data.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ingjimen <ingjimen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 10:00:56 by ingjimen          #+#    #+#             */
+/*   Updated: 2025/05/21 21:29:54 by ingjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+static bool     ft_check_argument(char *str)
+{
+    int     i;
+
+    i = 0;
+    while (ft_isspace(str[i]))
+        i++;
+    if (str[i] == '+')
+        i++;
+    if (!ft_is_digit(str[i]))
+        return (false);
+    if (str[i] == '+')
+        i++;
+    while (str[i])
+    {
+        if(!ft_is_digit(str[i]))
+            return (false);
+        i++;
+    }
+    return (true);
+}
+
+static void     ft_init_sim(t_sim *sim, char **argv)
+{
+    sim->num_of_philos = ft_atoi(argv[1]);
+    sim->time_to_die = ft_atoi(argv[2]);
+    sim->time_to_eat = ft_atoi(argv[3]);
+    sim->time_to_sleep = ft_atoi(argv[4]);
+    if (argv[5])
+        sim->number_of_times_each_philosopher_must_eat = ft_atoi(argv[5]);
+    else
+        sim->number_of_times_each_philosopher_must_eat = -1;
+    sim->someone_died = 0;
+    pthread_mutex_init(&sim->write_lock, NULL);
+	pthread_mutex_init(&sim->dead_lock, NULL);
+	pthread_mutex_init(&sim->meal_lock, NULL);
+}
+
+void	parse_data(char **argv, t_sim *table)
+{
+	int	i;
+
+	i = 1;
+	while (argv[i])
+	{
+		if (!ft_check_argument(argv[i]))
+			return (ft_error_parsing(table));
+		i++;
+	}
+	ft_init_sim(table, argv);
+	if (table->num_of_philos < 1 || table->num_of_philos > PHILO_MAX
+		|| table->time_to_die <= 0 || table->time_to_eat <= 0
+		|| table->time_to_sleep <= 0)
+		return (ft_error_parsing(table));
+	table->start_time = get_current_time_ms();
+}
+
