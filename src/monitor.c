308a6f66217df31349286f8a51d1c18c6bda79ce
MarcelaJI <ingridjimenez113@gmail.com>
2025-05-21 21:28:32 +0200

    Adding monitor.c

---
+++ src/monitor.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   monitor.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ingjimen <ingjimen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 22:01:41 by ingjimen          #+#    #+#             */
+/*   Updated: 2025/05/21 21:28:15 by ingjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+int	philo_died(t_philo *philo, size_t time)
+{
+	int		result;
+	long	current_time;
+
+	result = 0;
+	current_time = get_current_time_ms();
+	pthread_mutex_lock(&philo->sim->meal_lock);
+	if ((current_time - philo->last_meal >= time) && philo->eating == 0)
+		result = 1;
+	pthread_mutex_unlock(&philo->sim->meal_lock);
+	return (result);
+}
+
+int	check_death(t_sim *sim)
+{
+	int		i;
+
+	i = 0;
+	while (i < sim->num_of_philos)
+	{
+		if (philo_died(&sim->philos[i], sim->time_to_die))
+		{
+			pthread_mutex_lock(&sim->dead_lock);
+			if (!sim->someone_died)
+			{
+				sim->someone_died = 1;
+				print_status(&sim->philos[i], "died 💀", RED);
+			}
+			pthread_mutex_unlock(&sim->dead_lock);
+			return (EXIT_FAILURE);
+		}
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
+
+
+int	check_if_all_ate(t_sim *sim)
+{
+	int	i;
+	int	finished_eating;
+
+	if (sim->number_of_times_each_philosopher_must_eat == -1)
+		return (EXIT_FAILURE);
+	finished_eating = 0;
+	i = 0;
+	while (i < sim->num_of_philos)
+	{
+		pthread_mutex_lock(&sim->meal_lock);
+		if (sim->philos[i].meals_eaten >= sim->number_of_times_each_philosopher_must_eat)
+			finished_eating++;
+		pthread_mutex_unlock(&sim->meal_lock);
+		i++;
+	}
+	if (finished_eating == sim->num_of_philos)
+	{
+    	pthread_mutex_lock(&sim->dead_lock);
+    	sim->someone_died = 1;
+    	printf(RESET"%ld "YELLOW"All philosophers are full 🐷🥴🐷\n"RESET,
+        	get_current_time_ms() - sim->start_time);
+    	pthread_mutex_unlock(&sim->dead_lock);
+    	return (1);
+	}
+	return (EXIT_SUCCESS);
+}
+
+
+int philo_has_died(t_philo *philo)
+{
+    int result;
+
+    pthread_mutex_lock(&philo->sim->dead_lock);
+    result = philo->sim->someone_died;
+    pthread_mutex_unlock(&philo->sim->dead_lock);
+    return (result);
+}
+
+void *monitor_func(void *arg)
+{
+    t_sim *sim = (t_sim *)arg;
+    while (1)
+    {
+        if (check_death(sim) || check_if_all_ate(sim))
+            break;
+        usleep(1000);
+    }
+    return (NULL);
+}
