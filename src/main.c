f5c5afb5527919583f26406671985b66945002b1
MarcelaJI <ingridjimenez113@gmail.com>
2025-05-21 21:24:44 +0200

    Adding main

---
+++ src/main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ingjimen <ingjimen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/20 09:18:40 by ingjimen          #+#    #+#             */
+/*   Updated: 2025/05/21 21:24:36 by ingjimen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philo.h"
+
+int	main(int argc, char **argv)
+{
+	t_sim	sim;
+
+	if (argc != 5 && argc != 6)
+		error_exit("Wrong argument count\n");
+
+	parse_data(argv, &sim);
+	init_forks(&sim);
+	init_philos(&sim);
+	start_threads(&sim);
+	clean_up_all(NULL, &sim);
+	return (EXIT_SUCCESS);
+}
